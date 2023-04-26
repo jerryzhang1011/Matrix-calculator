81e85cef840565fefa85b0f2b417a463ca956ca4
jerryzhang1011 <131218857+jerryzhang1011@users.noreply.github.com>
2023-04-26 12:21:07 -0600

    updated add and remove function

--- matrix/io_opers.h
+++ matrix/io_opers.h
@@ -4,4 +4,5 @@ void matrix_io_driver();
 void get_matrix(int size, int matrix[10][10]);
 int get_matrix_size();
 int calculate_determinant(int size, int matrix[10][10]);
+void matrix_add_sub(int size, int(*func)(int, int));
 void cal_det_io(int size);
